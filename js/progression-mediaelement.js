e5d190bd4d0b8c00cbb31eb487342115088d50e9
Jan <m@jancb.de>
2013-07-17 14:24:48 +0200

    added custom options to mediaelement init

---
+++ js/progression-mediaelement.js
@@ -0,0 +1,12 @@
+(function($) {
+    // add mime-type aliases to MediaElement plugin support
+    mejs.plugins.silverlight[0].types.push('video/x-ms-wmv');
+    mejs.plugins.silverlight[0].types.push('audio/x-ms-wma');
+
+    $(function() {
+        $('.wp-audio-shortcode, .wp-video-shortcode').mediaelementplayer({
+        	startVolume: window.progression.startvolume // initial volume when the player starts
+        });
+    });
+
+}(jQuery)); 
