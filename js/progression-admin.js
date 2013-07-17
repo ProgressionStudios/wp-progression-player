c449e650d27ad3f96e7f26404bc4e310d57bcb4f
Jan <m@jancb.de>
2013-07-17 15:02:12 +0200

    added custom colors toggle function on settings page

--- js/progression-admin.js
+++ js/progression-admin.js
@@ -1,6 +1,23 @@
 (function ($) {
 	"use strict";
 	$(function () {
+
+		// init color picker on plugin settings page
 		$('.progression-skincolor').wpColorPicker();
+
+
+		function toggleCustomSettings() {
+			$( '.progression-skincolor' ).closest('tr')[$('#progression_custom_skin').is(':checked') ? 'show' : 'hide']();
+		}
+
+		// This toggles the custom color settings fields when the checkbox is set
+		$('#progression_custom_skin')
+			
+			// check on load
+			.each(toggleCustomSettings)
+
+			// check on change
+			.change(toggleCustomSettings);
+
 	});
 }(jQuery));
