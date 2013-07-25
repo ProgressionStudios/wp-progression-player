3f6e9b73173eb67f3896672c72b5ee10b2960413
Jan <m@jancb.de>
2013-07-25 11:18:44 +0200

    implemented the playlist select functionality in media library

--- js/progression-playlist.js
+++ js/progression-playlist.js
@@ -1,26 +1,5 @@
 (function($) {
 
-    // ---------------------------------
-    wp.media.controller.PlaylistAdd = wp.media.controller.Library.extend({
-        defaults: _.defaults({
-            id: 'playlist-library',
-            filterable: 'uploaded',
-            multiple: 'add',
-            menu: 'default',
-            editable: true,
-            toolbar: 'playlist',
-            library: wp.media.query(_.defaults({
-                type: 'audio'
-            }, {})),
-            title: window.progression.menuitem,
-            priority: 100,
-        }, wp.media.controller.Library.prototype.defaults),
-
-        initialize: function() {
-            wp.media.controller.Library.prototype.initialize.apply(this, arguments);
-        }
-    });
-
     // supersede the default MediaFrame.Post view
     var oldMediaFrame = wp.media.view.MediaFrame.Post;
     wp.media.view.MediaFrame.Post = wp.media.view.MediaFrame.Post.extend({
@@ -31,32 +10,35 @@
             var strings = window.progression;
 
             this.states.add([
-            new wp.media.controller.PlaylistAdd()]);
+                new wp.media.controller.Library({
+                    id: 'playlist-library',
+                    filterable: 'uploaded', // allows the user to show only the files uploaded to the current post
+                    multiple: true, // selecting more than one file makes sense for a playlist
+                    menu: 'default', // extend the default menu
+                    editable: true, // allow selection to be edited via drag & drop
+                    toolbar: 'playlist', // select our custom toolbar
+                    library: wp.media.query(_.defaults({
+                        type: 'audio' // allow audio files only
+                    }, {})),
+                    title: window.progression.menuitem,
+                    priority: 100,
+                })]
+            );
 
-            this.bindHandlers();
-
-            this.on('playlist', this.insert, this);
-
-        },
-
-        bindHandlers: function() {
-            wp.media.view.MediaFrame.Select.prototype.bindHandlers.apply(this, arguments);
+            // event binding
+            this.on('playlist:insert', this.insert, this);
             this.on('toolbar:create:playlist', this.createToolbar, this);
             this.on('toolbar:render:playlist', this.renderToolbar, this);
-        },
 
-        createToolbar: function(toolbar) {
-            toolbar.view = new wp.media.view.Toolbar({
-                controller: this
-            });
         },
 
         renderToolbar: function(view) {
             var controller = this;
 
+            // add the little view where we can edit the selection
             this.selectionStatusToolbar(view);
 
-            view.set('playlist', {
+            view.set('playlist-insert', {
                 style: 'primary',
                 text: window.progression.button,
                 priority: 60,
@@ -68,12 +50,13 @@
                     var state = controller.state(),
                         selection = state.get('selection');
 
-                    controller.close();
-                    state.trigger('playlist', selection).reset();
+                    controller.close(); // should close the window after we have selected
+                    state.trigger('playlist:insert', selection).reset();
                 }
             });
         },
 
+        // triggered when "insert" button is clicked
         insert: function() {
 
             var attrs = {},
@@ -90,6 +73,7 @@
             });
 
             // send shortcode back to editor
+            // used to be send_to_editor()
             wp.media.editor.insert(shortcode);
 
         }
